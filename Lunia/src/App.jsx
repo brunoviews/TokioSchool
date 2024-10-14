a97f11de2e1d4abc8c0c5e4f789a8bf8b53745b4
Harlxck <brunogustavo68@gmail.com>
2024-10-14 11:00:12 +0200

    Añadido Cookie Banner

--- Lunia/src/App.jsx
+++ Lunia/src/App.jsx
@@ -13,9 +13,9 @@ import SobreNosotros from "./pages/SobreNosotros";
 import Servicios from "./pages/Servicios";
 import Registro from "./pages/Registro";
 import { AnimatePresence } from "framer-motion";
+import CookieConsent from "react-cookie-consent";
+import "./components/CookieBanner.css";
 import "./App.css";
-
-
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -38,6 +38,29 @@ function App() {
     <Router>
       <NavBar />
       <AnimatedRoutes />
+      <CookieConsent 
+      location="bottom"
+      buttonText="Aceptar"
+      enableDeclineButton={true}
+      declineButtonText="Rechazar"
+      cookieName="cookieConsent"
+      containerClasses="my-banner" 
+      buttonClasses="my-button"     
+      declineButtonClasses="my-decline-button"  
+      expires={150}
+        onAccept={() => {
+          // Acciones al aceptar cookies
+          console.log("Cookies aceptadas");
+        }}
+        onDecline={() => {
+          // Acciones al rechazar cookies (opcional)
+          console.log("Cookies rechazadas");
+        }}
+      >
+        Este sitio web utiliza cookies para mejorar tu experiencia.{" "}
+        <span>Puedes cambiar tu configuración en cualquier momento.</span>
+      </CookieConsent>
+
       <Footer />
     </Router>
   );
